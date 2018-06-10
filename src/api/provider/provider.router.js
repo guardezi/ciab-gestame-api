c73c10334f0a3981485e1628b3376531deac4d98
guardezi <jefersonguardezi@gmail.com>
2018-06-10 02:37:36 -0300

    fix post api

--- src/api/provider/provider.router.js
+++ src/api/provider/provider.router.js
@@ -7,5 +7,6 @@ const router = express.Router()
 // router.get('/', getServices)
 // router.get('/:id', getUserById)
 router.get('/', getProviders)
+router.post('/', getProviders)
 
 export default router
